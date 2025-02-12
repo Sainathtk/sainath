deb8a4d5e1d314c31a44cb9ef390df05b1297238
Sainathtk <142080079+Sainathtk@users.noreply.github.com>
2025-02-12 15:10:30 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "467.devops.candidate.exam"   # S3 bucket name
+    key    = "<sainath>.<kulsange>"  # S3 key path for storing state
+    region = "ap-south-1"  # AWS Region for the S3 bucket
+  }
+}
